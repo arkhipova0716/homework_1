dddd2a5612ded91c950a9fc030b37ed1be78ef0a
Arkhipova <lilit0307@mail.ru>
2022-07-11 21:22:56 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+int a=5, b=7;
+if (a>b)
+{
+    Console.WriteLine($"Число {a} больше чем {b}");
+}
+else
+{
+    Console.WriteLine($"Число {b} больше чем {a}");
+}
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a, b, c;
+Console.WriteLine("Введите первое число - ");
+a = Convert.ToInt32(Console.Read());
+Console.WriteLine("Введите второе число - ");
+b = Convert.ToInt32(Console.Read());
+Console.WriteLine("Введите третье число - ");
+с = Convert.ToInt32(Console.Read());
