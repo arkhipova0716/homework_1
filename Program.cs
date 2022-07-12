ece18a2de65b67e63b02609d66bf52ccff196891
Arkhipova <lilit0307@mail.ru>
2022-07-12 19:16:34 +0300

    last tasks

--- Program.cs
+++ Program.cs
@@ -16,3 +16,43 @@ Console.WriteLine("Введите второе число - ");
 b = Convert.ToInt32(Console.Read());
 Console.WriteLine("Введите третье число - ");
 с = Convert.ToInt32(Console.Read());
+if (c<a>b)
+{
+    Console.WriteLine($"Число {a} максимальное");
+}
+if(c<b>a)
+{
+    Console.WriteLine($"Число {b} максимальное");
+}
+else
+{
+    Console.WriteLine($"Число {c} максимальное");
+}
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+int x=a;
+while (x<b)
+{
+   if (a%2==0)
+      {
+         Console.WriteLine($"Число {a} четное");
+      }
+   x++;
+}
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+int a;                             
+            Console.Write("введите число: ");
+            a= Console.Read();
+ 
+            if (a % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
